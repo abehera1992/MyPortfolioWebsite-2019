fcd0a274553cfbfe65770760b965387ed55d6469
Abhishek Behera <abehera1992@gmail.com>
2019-05-18 11:32:04 -0400

    nav-link jquery added for active state

--- js/main.js
+++ js/main.js
@@ -1,1 +1,34 @@
-var scroll = new SmoothScroll('a[href*="#"]');
+
+    var scroll = new SmoothScroll('a[href*="#"]');
+
+    $('#homeTab').on('click', function (e) {
+      
+        $(this).addClass('active'),
+        $('#aboutTab').removeClass('active'),
+        $('#resumeTab').removeClass('active'),
+        $('#contactTab').removeClass('active');
+      });
+      $('#aboutTab').on('click', function (e) {
+   
+        $(this).addClass('active'),
+        $('#homeTab').removeClass('active'),
+        $('#resumeTab').removeClass('active'),
+        $('#contactTab').removeClass('active');
+      });
+      $('#resumeTab').on('click', function (e) {
+    
+        $(this).addClass('active'),
+        $('#homeTab').removeClass('active'),
+        $('#aboutTab').removeClass('active'),
+        $('#contactTab').removeClass('active');
+      });
+      $('#contactTab').on('click', function (e) {
+   
+        $(this).addClass('active'),
+        $('#homeTab').removeClass('active'),
+        $('#aboutTab').removeClass('active'),
+        $('#resumeTab').removeClass('active');
+      });
+      
+
+
